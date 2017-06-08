f8741e01d69266e26f1be80f582782ed64db2671
Simon Ho <superkhau@gmail.com>
2017-06-08 13:14:30 -0700

    Create index.ts

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,9 +1,7 @@
-import {handleHttp, handleHttps} from '@loopback/handlers'
+import {createApp} from './application'
 import http from 'http'
 import https from 'https'
-import {createApp} from './application'
 
-const app = createApp()
-
-http.createServer(handleHttp(app, {httpsRedirect: true})).listen(80);
-https.createServer(handleHttps(app)).listen(443);
+const app = createApp();
+http.createServer(app.handleHttp({httpsRedirect: true})).listen(80);
+https.createServer(app.handleHttps()).listen(443);
