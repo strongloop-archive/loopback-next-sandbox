7b4f4a6f2cb42c3db10871d5c24ba34187b971d6
Simon Ho <superkhau@gmail.com>
2017-06-08 13:16:54 -0700

    Create index.ts

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,6 +1,6 @@
 import {createApp} from './application';
-import http from 'http';
-import https from 'https';
+import * as http from 'http';
+import * as https from 'https';
 
 const app = createApp();
 http.createServer(app.httpHandler({httpsRedirect: true})).listen(80);
